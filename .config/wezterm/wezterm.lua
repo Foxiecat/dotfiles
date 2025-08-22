4cfec4434a5e28262f6662435f7a41572c491d17
Danica Ellefsen Kvilhaug <foxiecat+programming@pm.me>
2025-08-22 08:51:13 +0200

    st

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,24 +1,43 @@
 -- Pull in the wezterm API
 ---@type Wezterm
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 -- This will hold the configuration.
----@type Config
-local config = {}
-config = wezterm.config_builder()
+---@class Config
+local config = wezterm.config_builder()
 
-
-
+config.automatically_reload_config = false
+config.enable_wayland = true
 
 -- Colors
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "catppuccin-mocha"
+
+-- Font
+config.font = wezterm.font("JetBrainsMono NF")
+
+-- Window
+config.initial_cols = 160
+config.initial_rows = 40
+
+config.window_background_opacity = 0.5
+config.kde_window_background_blur = true
+
+--config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = true
 
 -- Appearance
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
-config.window_decorations = "RESIZE"
---config.hide_tab_bar_if_only_one_tab = true
+--config.background = {
+--	source = {
+--		File = '/home/foxiecat/Pictures/Maids/SlimeMaid3.png',
+--	},
+--	height = 'Cover',
+--	width = 'Cover',
+--	repeat_x = 'NoRepeat',
+--	repeat_y = 'NoRepeat',
+--}
 
 -- and finally, return the configuration to wezterm
 return config
