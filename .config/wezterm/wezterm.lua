d4d4e96cc7ae62c084f04af33399661b872ae166
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2025-10-30 10:08:11 +0100

    Implemented keybinds.lua & system_specific.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,52 +1,39 @@
 -- Pull in the wezterm API
 ---@type Wezterm
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
+local keybinds = require 'keybinds'
+local system = require 'system_specific'
 local act = wezterm.action
+
+local is_linux = function()
+	return wezterm.target_triple:find("linux") ~= nil
+end
+local is_mac = function()
+	return wezterm.target_triple:find("darwin") ~= nil
+end
 
 -- This will hold the configuration.
 ---@class Config
 local config = wezterm.config_builder()
 
-config.automatically_reload_config = false
---config.enable_wayland = true
+-- System Specific Configuration:
+if is_mac then
+	system.mac_config(config)
+end
 
+-- Non-System Specific Configuration:
 -- Colors
 config.color_scheme = "catppuccin-mocha"
 
 -- Font
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 
--- Window
-config.window_background_opacity = 0.5
-config.macos_window_background_blur = 50
-
-config.window_decorations = "RESIZE"
-config.window_close_confirmation = "NeverPrompt"
-config.hide_tab_bar_if_only_one_tab = true
+-- Tabs
 config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
 
-config.send_composed_key_when_left_alt_is_pressed = true
-config.send_composed_key_when_right_alt_is_pressed = false
-config.keys = {
-	{
-		key = '.',
-		mods = 'ALT',
-		action = act.SplitPane {
-			direction = 'Right'
-		},
-	},
-	{
-		key = ',',
-		mods = 'ALT',
-		action = act.SplitPane {
-			direction = 'Down'
-		},
-	},
-	{
-		key = 'x',
-		mods = 'ALT',
-		action = act.CloseCurrentPane { confirm = false },
-	},
-}
+-- Keybinds
+keybinds.keys(config)
+
 -- and finally, return the configuration to wezterm
 return config
