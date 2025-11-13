4aea4c5bc158b072351352c2de8b722d8e74f461
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2025-11-13 09:40:19 +0100

    Added CurrentPaneDomain

--- .config/wezterm/keybinds.lua
+++ .config/wezterm/keybinds.lua
@@ -1,30 +1,32 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 local module = {}
 
 function module.keys(config)
 	config.keys = {
-        {
-                key = '.',
-                mods = 'ALT',
-                action = act.SplitPane {
-                        direction = 'Right'
-                },
-        },
-        {
-                key = ',',
-                mods = 'ALT',
-                action = act.SplitPane {
-                        direction = 'Down'
-                },
-        },
-        {
-                key = 'x',
-                mods = 'ALT',
-                action = act.CloseCurrentPane { confirm = false },
-        },
-}
+		{
+			key = ".",
+			mods = "ALT",
+			action = act.SplitPane({
+				direction = "Right",
+				command = { domain = "CurrentPaneDomain" },
+			}),
+		},
+		{
+			key = ",",
+			mods = "ALT",
+			action = act.SplitPane({
+				direction = "Down",
+				command = { domain = "CurrentPaneDomain" },
+			}),
+		},
+		{
+			key = "x",
+			mods = "ALT",
+			action = act.CloseCurrentPane({ confirm = false }),
+		},
+	}
 end
 
 return module
